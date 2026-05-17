d776f6301b3067af7ee2fbb5e431b759b4d3817f
Ian Christopher Villegas <kyia1nvillegas@gmail.com>
2026-05-17 16:18:35 +0800

    Revert "Added remainder operation"
    This reverts commit 879a9809b1a5da6b3fcf1f62e40b1cd26616f2cc.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -25,9 +25,6 @@ class Calculator
                     result = num1 / num2;
                 }
                 break;
-            case "r":
-                result = num1 % num2;
-                break;
             // Return text for an incorrect option entry.
             default:
                 break;
@@ -81,7 +78,6 @@ class Program
             Console.WriteLine("\ts - Subtract");
             Console.WriteLine("\tm - Multiply");
             Console.WriteLine("\td - Divide");
-            Console.WriteLine("\tr - Remainder");
             Console.Write("Your option? ");
 
             string? op = Console.ReadLine();
